bb53642e4add6d6f63d847ad06e23810cc1b55e4
ga96sag <ga96sag@tum.de>
2018-08-30 17:51:56 -0400

    Complete crawling solution: GDELT crawler done

--- src/data/GDELT_Event_2/sqlite_import.py
+++
@@ -1,7 +0,0 @@
-"""
-This script extracts the relevant GDELT data and puts it into the sqlite database.
-"""
-
-TABLE_NAME = "gdelt"
-
-columns = ["GLOBALEVENTID", "EventCode", "EventBaseCode", "EventRootCode"]
