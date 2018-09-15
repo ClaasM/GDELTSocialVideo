a7130e94b50baa9ae8ae6e799347e6532d4b7950
ga96sag <ga96sag@tum.de>
2018-09-15 11:22:35 -0400

    DB now initializing without errors

--- src/data/postgres/sql/init_db.sql
+++ src/data/postgres/sql/init_db.sql
@@ -7,28 +7,8 @@ Then run this script to initialize it.
 */
 
 -- TABLES
-CREATE TABLE IF NOT EXISTS Mentions (
-  -- Columns from the dataset
-  global_event_id     BIGINT NOT NULL,
-  event_time_date     BIGINT NOT NULL,
-  mention_time_date   BIGINT NOT NULL,
-  mention_type        INT    NOT NULL,
-  mention_source_name TEXT   NOT NULL,
-  mention_identifier  TEXT   NOT NULL,
-  sentence_id         INT    NOT NULL,
-  actor1_char_offset  INT    NOT NULL,
-  actor2_char_offset  INT    NOT NULL,
-  action_char_offset  INT    NOT NULL,
-  in_raw_text         BOOL   NOT NULL,
-  confidence          FLOAT  NOT NULL,
-  mention_doc_len     INT    NOT NULL,
-  mention_doc_tone    FLOAT  NOT NULL,
 
-  FOREIGN KEY (global_event_id) REFERENCES Export (global_event_id),
-  FOREIGN KEY (mention_source_name) REFERENCES Sources (source_name)
-);
-
-CREATE TABLE IF NOT EXISTS Export (
+CREATE TABLE IF NOT EXISTS export (
   -- Event ID and date attributes
   global_event_id          BIGINT NOT NULL,
   sql_date                 INT    NOT NULL,
@@ -101,23 +81,23 @@ CREATE TABLE IF NOT EXISTS Export (
   PRIMARY KEY (global_event_id)
 );
 
-CREATE TABLE IF NOT EXISTS Articles (
+CREATE TABLE IF NOT EXISTS articles (
   source_url          TEXT NOT NULL, -- Those are the mention_identifiers that are url's (they not always are, see GDELT docs)
   crawling_status     TEXT DEFAULT 'Not Crawled',
 
   PRIMARY KEY (source_url) -- Primary keys are automatically indexed
 );
 
-CREATE TABLE IF NOT EXISTS FoundVideos (
+CREATE TABLE IF NOT EXISTS found_videos (
   source_url TEXT NOT NULL,
   platform   TEXT NOT NULL,
   video_url  TEXT NOT NULL,
   video_id   TEXT NOT NULL, --the video_id is extracted from the url to make querying for it faster.
 
-  FOREIGN KEY (source_url) REFERENCES Articles (source_url)
+  FOREIGN KEY (source_url) REFERENCES articles (source_url)
 );
 
-CREATE TABLE Sources (
+CREATE TABLE sources (
   source_name                 TEXT,
   article_count               INT,
   twitter_video_std_dev       FLOAT,
@@ -140,27 +120,46 @@ CREATE TABLE Sources (
   PRIMARY KEY (source_name)
 );
 
+CREATE TABLE IF NOT EXISTS mentions (
+  -- Columns from the dataset
+  global_event_id     BIGINT NOT NULL,
+  event_time_date     BIGINT NOT NULL,
+  mention_time_date   BIGINT NOT NULL,
+  mention_type        INT    NOT NULL,
+  mention_source_name TEXT   NOT NULL,
+  mention_identifier  TEXT   NOT NULL,
+  sentence_id         INT    NOT NULL,
+  actor1_char_offset  INT    NOT NULL,
+  actor2_char_offset  INT    NOT NULL,
+  action_char_offset  INT    NOT NULL,
+  in_raw_text         BOOL   NOT NULL,
+  confidence          FLOAT  NOT NULL,
+  mention_doc_len     INT    NOT NULL,
+  mention_doc_tone    FLOAT  NOT NULL,
+
+  FOREIGN KEY (global_event_id) REFERENCES export (global_event_id),
+  FOREIGN KEY (mention_source_name) REFERENCES sources (source_name)
+);
+
 -- INDICES
 -- Indices are only created where they are really needed, because they take up space and slow down inserts/deletes
 CREATE INDEX mentions_global_event_id_index
-  ON public.Mentions (global_event_id);
+  ON public.mentions (global_event_id);
 CREATE INDEX mentions_mention_identifier_index
-  ON public.Mentions (mention_identifier);
+  ON public.mentions (mention_identifier);
 CREATE INDEX export_source_url_index
-  ON public.Export (source_url);
+  ON public.export (source_url);
 CREATE INDEX articles_crawling_status_index
-  ON Articles (crawling_status);
-CREATE INDEX articles_mention_source_name_index
-  ON Articles (mention_source_name);
+  ON public.articles (crawling_status);
 CREATE INDEX found_videos_platform_index
-  ON FoundVideos (platform);
+  ON public.found_videos (platform);
 CREATE INDEX found_videos_video_url_index
-  ON FoundVideos (video_url);
+  ON public.found_videos (video_url);
 CREATE INDEX found_videos_video_id_index
-  ON FoundVideos (video_id);
+  ON public.found_videos (video_id);
 CREATE INDEX source_twitter_relevant_index
-  ON Sources (twitter_relevant);
+  ON sources (twitter_relevant);
 CREATE INDEX source_youtube_relevant_index
-  ON Sources (youtube_relevant);
+  ON sources (youtube_relevant);
 CREATE INDEX source_facebook_relevant_index
-  ON Sources (facebook_relevant);
+  ON sources (facebook_relevant);
