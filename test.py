125137c35c8c006bd2eb5f57edd72852580133ed
ga96sag <ga96sag@tum.de>
2018-10-21 13:45:25 -0400

    Fixed the issue of flipped video_ids for facebook videos, and some other small stuff.

--- test.py
+++ test.py
@@ -1,52 +1,28 @@
 # crawl any websites that dont contain </html>
-import re
-from multiprocessing.pool import Pool
 
-import htmlmin
 import psycopg2
-import sys
 
-import requests
-
-from src.data.videos import video as video_helper
-from src.data.websites import website as website_helper
 from src.visualization.console import CrawlingProgress
 
-minifier = htmlmin.Minifier(remove_comments=True, remove_all_empty_space=True, reduce_boolean_attributes=True,
-                            remove_empty_space=True)
 
-def crawl_article(article):
-    url, = article
-    try:
-        text = website_helper.load(url)
-        if "</html>" not in text:
-            res = requests.get(url, headers={"user-agent": "Mozilla"})
-            if res.status_code >= 300:
-                print(res.status_code)
-                pass
-            else:
-                print("Success!")
-                website_helper.save(minifier.minify(res.text), url)
-    except Exception as e:
-        # The website was not successfully crawled, it should be tried again
-        #print(e)
-        return str(e)
-
-    return "Success"
 
 def run():
     conn = psycopg2.connect(database="gdelt_social_video", user="postgres")
 
     c = conn.cursor()
-    c.execute("SELECT DISTINCT source_url from article_videos")
-    articles = c.fetchall()
+    c.execute("SELECT DISTINCT video_id from article_videos WHERE platform='facebook'")
+    videos = c.fetchall()
 
-    crawling_progress = CrawlingProgress(len(articles), update_every=10000)
-
-    # parallel crawling and parsing to speed things up
-    with Pool(32)  as pool:  # 16 seems to be around optimum
-        for _ in pool.imap_unordered(crawl_article, articles, chunksize=100):
-            crawling_progress.inc(1)
+    for video_id, in videos:
+        split = video_id.split("/")
+        new_video_id = split[1] + "/" + split[0]
+        c.execute("SELECT count(*) FROM videos WHERE id=%s AND platform='facebook'", [new_video_id])
+        count, = c.fetchone()
+        if count == 1:
+            # This is one of the video_ids that are flipped
+            c.execute("UPDATE article_videos SET video_id=%s WHERE video_id=%s AND platform='facebook'", [new_video_id, video_id])
+            conn.commit()
+    # Video_id for facebook videos in article_videos are flipped for some reason.
 
 
 if __name__ == "__main__":
